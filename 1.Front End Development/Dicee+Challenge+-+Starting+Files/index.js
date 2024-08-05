3dedd1e5f20881c357d18978034db9bfa5ae54c0
menaujiri <ujirimena@gmail.com>
2024-08-05 06:32:34 +0100

    update

--- 1.Front End Development/Dicee+Challenge+-+Starting+Files/index.js
+++ 1.Front End Development/Dicee+Challenge+-+Starting+Files/index.js
@@ -1,2 +1,11 @@
 var randomNumber1 = Math.floor(Math.random()*6) +1 ;
+var imageNumber1 = "dice" + randomNumber1 + ".png";
+var imageSource = "images/" + imageNumber1;
+
+var image1 = document.querySelector(".img1");
+image1.setAttribute("src", imageSource);
+
+
+
+
 
