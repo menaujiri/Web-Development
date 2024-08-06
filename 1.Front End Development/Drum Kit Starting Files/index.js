be0ea4e67862653547c7faf5cc5b2b22a5b969ab
menaujiri <ujirimena@gmail.com>
2024-08-06 05:07:55 +0100

    Update

--- 1.Front End Development/Drum Kit Starting Files/index.js
+++ 1.Front End Development/Drum Kit Starting Files/index.js
@@ -1,1 +1,11 @@
-function 
+
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for (i = 0, i<numberOfDrums, i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        alert("I got clicked");
+    })
+}
+
+
+
