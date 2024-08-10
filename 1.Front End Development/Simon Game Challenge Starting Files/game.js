1d37edba97459071922f9651cd791bb73a4700e5
menaujiri <ujirimena@gmail.com>
2024-08-10 06:14:03 +0100

    Update

--- 1.Front End Development/Simon Game Challenge Starting Files/game.js
+++ 1.Front End Development/Simon Game Challenge Starting Files/game.js
@@ -1,15 +1,25 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = []
+
+$(".btn" ).click(function() {
+    var userChosenColour = $(this).attr("id")
+    userClickedPattern.push(userChosenColour)
+    playSound(userChosenColour)
+    
+  } );
+
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenNumber = buttonColours[randomNumber];
     gamePattern.push(randomChosenNumber);
     $("#" + randomChosenNumber).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio('audio_file.mp3');
-    audio.play();
-
-
+    playSound(randomChosenColour)
 }
 
-nextSequence();
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
