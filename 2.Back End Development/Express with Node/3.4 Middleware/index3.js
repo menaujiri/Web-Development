7e82af60439cea9aa017cd27aeaadb9d629bcc32
menaujiri <ujirimena@gmail.com>
2024-08-18 06:21:05 +0100

    Middleware

--- 2.Back End Development/Express with Node/3.4 Middleware/index3.js
+++ 2.Back End Development/Express with Node/3.4 Middleware/index3.js
@@ -5,12 +5,13 @@ const app = express();
 const port = 3000;
 
 function logger(req, res, next) {
-  console.log("Request Method: " req.method);
-  console.log("URL Method: " req.url);
+  console.log("Request Method: ", req.method);
+  console.log("Request URL: ", req.url);
   next();
 }
 
 app.use(logger);
+
 
 app.get("/", (req, res) => {
   res.send("Hello");
