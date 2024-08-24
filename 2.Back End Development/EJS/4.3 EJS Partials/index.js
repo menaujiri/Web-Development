c8aab1e9335c250152dc43a9c1e56f11d3ebba9b
menaujiri <ujirimena@gmail.com>
2024-08-24 12:27:13 +0100

    partials

--- 2.Back End Development/EJS/4.3 EJS Partials/index.js
+++ 2.Back End Development/EJS/4.3 EJS Partials/index.js
@@ -1,7 +1,24 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
 
 /* Write your code here:
 Step 1: Render the home page "/" index.ejs
