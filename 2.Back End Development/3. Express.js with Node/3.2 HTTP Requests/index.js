e4b4bdb591ae0af6189abfa3fac34dd10e1e493c
menaujiri <ujirimena@gmail.com>
2024-09-03 05:02:30 +0100

    create http requests

--- 2.Back End Development/3. Express.js with Node/3.2 HTTP Requests/index.js
+++ 2.Back End Development/3. Express.js with Node/3.2 HTTP Requests/index.js
@@ -3,7 +3,15 @@ const app = express()
 const port = 3000
 
 app.get("/", (req, res) => {
-    res.send("Hello Wold")
+    res.send("<h1>Hello World</h1>")
+})
+
+app.get("/about", (req, res) => {
+    res.send("<h1>About me</h1><p>My name is mena Ujiri</>")
+})
+
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact me</h1><p>This is my phone number</p>")
 })
 
 app.listen(port, () => {
