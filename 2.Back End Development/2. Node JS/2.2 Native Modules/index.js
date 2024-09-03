b037b20b0647a813032b1d5f4deedca1a80518b4
menaujiri <ujirimena@gmail.com>
2024-09-03 01:57:45 +0100

    comment

--- 2.Back End Development/2. Node JS/2.2 Native Modules/index.js
+++ 2.Back End Development/2. Node JS/2.2 Native Modules/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
-fs.writeFile("message.txt", "Hello from nodeJS. Today is great", (err) => {
-  if (err) throw err;
-  console.log ("The file has been saved!");
-});
+// fs.writeFile("message.txt", "Hello from nodeJS. Today is great", (err) => {
+//   if (err) throw err;
+//   console.log ("The file has been saved!");
+// });
+
