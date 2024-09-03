9ecc72acfc25270758c89d828e5130ec8427c316
menaujiri <ujirimena@gmail.com>
2024-09-03 04:32:53 +0100

    install nodemon

--- 2.Back End Development/3. Express.js with Node/3.2 HTTP Requests/index.js
+++ 2.Back End Development/3. Express.js with Node/3.2 HTTP Requests/index.js
@@ -1,6 +1,10 @@
 import express from "express";
-const app = epress()
+const app = express()
 const port = 3000
+
+app.get("/", (req, res) => {
+    res.send("Hello Wold")
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
