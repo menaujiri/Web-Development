077cabbf067de030349bc5fee1e939be35379751
menaujiri <ujirimena@gmail.com>
2024-09-12 01:50:17 +0100

    partials

--- 2.Back End Development/4. EJS/4.3 EJS Partials/index.js
+++ 2.Back End Development/4. EJS/4.3 EJS Partials/index.js
@@ -3,6 +3,16 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+
+
+
+
 /* Write your code here:
 Step 1: Render the home page "/" index.ejs
 Step 2: Make sure that static files are linked to and the CSS shows up.
