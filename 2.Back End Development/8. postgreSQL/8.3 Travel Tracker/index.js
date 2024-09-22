6b6f611ff920da9d1ba8f6581e4c3266fffaa6db
menaujiri <ujirimena@gmail.com>
2024-09-22 04:45:08 +0100

    Update

--- 2.Back End Development/8. postgreSQL/8.3 Travel Tracker/index.js
+++ 2.Back End Development/8. postgreSQL/8.3 Travel Tracker/index.js
@@ -1,14 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
+import db from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "justdoit",
+  post: 5432,
+});
+db.connect();
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  //Write your code here.
+  
 });
 
 app.listen(port, () => {
