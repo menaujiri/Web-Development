25aff892f7e3eb15fd85cfc498712dadb4757283
menaujiri <ujirimena@gmail.com>
2024-09-25 03:16:27 +0100

    add new files

--- 3. React/my-react-app/src/main.jsx
+++ 3. React/my-react-app/src/main.jsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
+
+
+// ReactDOM.render(<hi>Hello World</hi>, document.getElementById("root"))
