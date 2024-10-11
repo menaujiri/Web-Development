6ca748e3e1e12218c7862d7e566450353f8f061f
menaujiri <ujirimena@gmail.com>
2024-10-11 05:23:27 +0100

    update

--- 3. React learn/300-map-filter-reduce/src/index.jsx
+++ 3. React learn/300-map-filter-reduce/src/index.jsx
@@ -2,11 +2,43 @@ var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
 
+// function double(x) {
+//     return(x * 2);
+// }
+
+// const newNumbers = numbers.map(double);
+
+// console.log(newNumbers)
+
+
 //Filter - Create a new array by keeping the items that return true.
+
+// function greaterThan(num) {
+//     return (num > 10);
+// }
+// const newNumbers = numbers.filter(function (num) {
+//     return num > 10;
+// });
+
+// console.log(newNumbers)
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 
+// var newNuymber = numbers.reduce(function (accumulator, currentNumber) {
+//     console.log ("accumulator=" + accumulator)
+//     console.log ("currentNumber=" + currentNumber)
+
+//     return accumulator + currentNumber
+// })
+
+// console.log(newNuymber)
+
+var numbers = [3, 56, 2, 48, 5];
 //Find - find the first item that matches from an array.
+const newNumber = numbers.findIndex(function (num) {
+    return num > 10;
+})
+ console.log(newNumber)
 
 //FindIndex - find the index of the first item that matches.
 
